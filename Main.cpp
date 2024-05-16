45f71eaa23395005b3e40944e3de774d4d1604e1
Nastasi-Santino <nastasisantino04@gmail.com>
2024-05-16 16:25:23 -0300

    Agregue que si entra a un lugar sin salida (recto) no vuelva a entrar

--- Main.cpp
+++ Main.cpp
@@ -11,41 +11,36 @@ int main(void)
 
     Mouse myMouse = {{0, 0}, 0, 1, NORTH};
     Maze myMaze;
+    bool noExitPath = false;
 
     initMaze(myMaze);
 
     while (true)
     {
-        // if (!API::wallLeft())
-        // {
-        //     API::turnLeft();
-        //     turnMouseLeft(myMouse);
-        // }
-        // while (API::wallFront())
-        // {
-        //     API::turnRight();
-        //     turnMouseRight(myMouse);
-        // }
-
         switch(chooseNextStep(myMouse, myMaze)){
 
             case 'l':
+                noExitPath = false;
                 API::turnLeft();
                 turnMouseLeft(myMouse);
                 break;
 
             case 'r':
+                noExitPath = false;
                 API::turnRight();
                 turnMouseRight(myMouse);
                 break;
 
             case 'f':
+                if(noExitPath){
+                    myMaze.nodes[myMouse.pos.x * MAZE_SIZE + myMouse.pos.y].mark = true;
+                }
                 break;
 
             default:
                 // En este caso, el ratón encontró una pared en una celda adyacente
-                handleWallEncounter(myMouse, myMaze);
                 myMaze.nodes[myMouse.pos.x * MAZE_SIZE + myMouse.pos.y].mark = true;
+                noExitPath = true;
                 API::turnRight();
                 turnMouseRight(myMouse);
                 API::turnRight();
